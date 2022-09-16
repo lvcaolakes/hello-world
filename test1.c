6e4706940e001cbfa79549777c5a9e9bc032fb07
lvcaolakes <522874042@qq.com>
2022-09-16 13:00:44 +0800

    kaifa2,fix bug

--- test1.c
+++ test1.c
@@ -3,7 +3,7 @@ int main()
 {
     printf("develop 提交test");
     printf("kaifa1,add feature—B,fix bug");
-    printf("kanfa2,add feature-B");
+    printf("kanfa2,add feature-B,fix bug");
     reture 0;
 
 }
