e8eb96e22cccc05f9ce083958eb5892326ab2266
lvcaolakes <522874042@qq.com>
2022-09-16 14:05:12 +0800

    kaifa1,feature-b-1,fix bug

--- test1.c
+++ test1.c
@@ -4,7 +4,7 @@ int main()
     printf("develop 提交test");
     printf("kaifa1,add feature—B,fix bug");
     printf("kanfa2,add feature-B,fix bug");
-    printf("kaifa1,add feature-b-1");
+    printf("kaifa1,add feature-b-1,fix bug");
     printf("kaifa2,add feature-b-2");
     reture 0;
 
