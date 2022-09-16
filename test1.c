5584920ce2659229d9f9d67e64d01f67577309be
lvcaolakes <522874042@qq.com>
2022-09-16 14:17:19 +0800

    kaifa1,add featur-b-3

--- test1.c
+++ test1.c
@@ -6,6 +6,7 @@ int main()
     printf("kanfa2,add feature-B,fix bug");
     printf("kaifa1,add feature-b-1,fix bug");
     printf("kaifa2,add feature-b-2");
+    printt("kaifa1,add feature-b-3");
     reture 0;
 
 }
